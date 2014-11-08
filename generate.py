ef5199a6bc4777b2cc27754c9429fcbe65e31217
Josh Tan <jtan189@gmail.com>
2014-11-08 17:57:18 -0500

    simpler version of middle two same vowel requirement implemented.

--- generate.py
+++ generate.py
@@ -11,12 +11,18 @@ def generate_pw(n, pw, total, num_vowels):
         if len(pw) == 0 or len(pw) == total-1:
             low = 65
             high = 90
+        if (total % 2 == 0) and (n == (total/2)+1):
+            low = 65
+            high = 90
+        if (total % 2 == 0) and (n == (total/2)):
+            low = ord(pw[-1])
+            high = ord(pw[-1])
 
-        for i in range(low, high):
+        for i in range(low, high+1):
             num_vowels2 = num_vowels
             if i in vowels:
                 num_vowels2 = num_vowels+1
             pw2 = pw + chr(i)
             generate_pw(n-1, pw2, total, num_vowels2)
 
-generate_pw(5, "", 5, 0)
+generate_pw(4, "", 4, 0)
