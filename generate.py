8743822418b100cb83ee5f51ea23ffb0855e90d4
Josh Tan <jtan189@gmail.com>
2014-11-08 17:23:59 -0500

    Added first and last uppercase requirement.

--- generate.py
+++ generate.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
-def generate_pw(n, pw):
+def generate_pw(n, pw, total):
     if n == 0:
         print pw
     else:
-        for i in range(33, 126):
-            pw2 = pw + chr(i)
-            generate_pw(n-1, pw2)
+        if len(pw) == 0 or len(pw) == total-1:
+            for i in range(65, 90):
+                pw2 = pw + chr(i)
+                generate_pw(n-1, pw2, total)            
+        else: 
+            for i in range(33, 126):
+                pw2 = pw + chr(i)
+                generate_pw(n-1, pw2, total)
 
-generate_pw(5, "")
-
+generate_pw(5, "", 5)
