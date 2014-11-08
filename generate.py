7e2c055174b2fd52ea201776dab214b8eff8ebe9
Josh Tan <jtan189@gmail.com>
2014-11-08 17:38:59 -0500

    added vowel num requirement

--- generate.py
+++ generate.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
+vowels = [65, 69, 73, 79, 85, 97, 101, 105, 111, 117]
 
-def generate_pw(n, pw, total):
+def generate_pw(n, pw, total, num_vowels):
     if n == 0:
-        print pw
+        if num_vowels >= 3:
+            print pw
     else:
+        low = 33
+        high = 126
         if len(pw) == 0 or len(pw) == total-1:
-            for i in range(65, 90):
-                pw2 = pw + chr(i)
-                generate_pw(n-1, pw2, total)            
-        else: 
-            for i in range(33, 126):
-                pw2 = pw + chr(i)
-                generate_pw(n-1, pw2, total)
+            low = 65
+            high = 90
 
-generate_pw(5, "", 5)
+        for i in range(low, high):
+            num_vowels2 = num_vowels
+            if i in vowels:
+                num_vowels2 = num_vowels+1
+            pw2 = pw + chr(i)
+            generate_pw(n-1, pw2, total, num_vowels2)
+
+generate_pw(5, "", 5, 0)
